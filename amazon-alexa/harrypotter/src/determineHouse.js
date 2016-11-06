f8ab95253cb007499cd679f938c5543e36ea3d5f
yshin4 <yshin4@lion.lmu.edu>
2016-11-06 14:41:24 -0800

    added determineHouse function

--- amazon-alexa/harrypotter/src/determineHouse.js
+++ amazon-alexa/harrypotter/src/determineHouse.js
@@ -1,6 +1,12 @@
 
 function determineHouse(houseTally){
-  var highestValue = Math.max(houseTally.gryffindor, houseTally.slytherin, houseTally.hufflepuff, houseTally.ravenclaw);
-  var array
-  for()
+  var highestValue = Math.max(houseTally[0].points,houseTally[1].points,houseTally[2].points,houseTally[3].points);
+  var highestValueArray = [];
+  for(var i in houseTally){
+    if(houseTally[i].points === highestValue){
+      highestValueArray.push(houseTally[i].name);
+    }
+  }
+  console.log(highestValueArray[0]);
+  return highestValueArray[Math.floor(Math.random()*highestValueArray.length)];
 }
--- amazon-alexa/harrypotter/src/index.js
+++ amazon-alexa/harrypotter/src/index.js
@@ -108,6 +108,20 @@ exports.handler = function( event, context ) {
     }
 };
 
+
+function determineHouse(houseTally){
+  var highestValue = Math.max(houseTally[0].points,houseTally[1].points,houseTally[2].points,houseTally[3].points);
+  var highestValueArray = [];
+  for(var i in houseTally){
+    if(houseTally[i].points === highestValue){
+      highestValueArray.push(houseTally[i].name);
+    }
+  }
+  console.log(highestValueArray[0]);
+  return highestValueArray[Math.floor(Math.random()*highestValueArray.length)];
+}
+
+
 function buildSpeechletResponse(say, shouldEndSession) {
     return {
         outputSpeech: {
