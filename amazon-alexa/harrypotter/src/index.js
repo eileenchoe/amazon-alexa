d994d8f2595ad81c7748f9e7297a35b2582fed5b
yshin4 <yshin4@lion.lmu.edu>
2016-11-06 15:17:47 -0800

    formatted question/answer say

--- amazon-alexa/harrypotter/src/index.js
+++ amazon-alexa/harrypotter/src/index.js
@@ -8,9 +8,9 @@ var questions = [
     {
         "question": "If Death were to offer you a gift, which would you take?",
         "answers":{
-            "a":"Cloak of invisibility ",
-            "b":"Resurrection stone",
-            "c":"Elder Wand",
+            "a":"The Cloak of invisibility ",
+            "b":"The Resurrection stone",
+            "c":"The Elder Wand",
         }
     },
     {
@@ -83,7 +83,8 @@ exports.handler = function( event, context ) {
         var IntentName = event.request.intent.name;
         var questionIndex = 0;
         if (IntentName === "BeginningGameIntent") {
-            say="Let's Begin! " + questions[questionIndex].question;
+            say="Let's Begin! " + questions[questionIndex].question + "? A. " + questions[questionIndex].answers.a + ". B. " +
+            questions[questionIndex].answers.b + ". Or C. " + questions[questionIndex].answers.c + ".";
             context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
         } else if (IntentName === "AnswerIntentA" || IntentName === "AnswerIntentB" || IntentName === "AnswerIntentC"){
             addPoints(intentName, currentQuestionIndex);
@@ -120,6 +121,62 @@ function determineHouse(houseTally){
   console.log(highestValueArray[0]);
   return highestValueArray[Math.floor(Math.random()*highestValueArray.length)];
 }
+
+
+var addPoints = function(intentName, currentQuestionIndex){
+    switch(currentQuestionIndex){
+        case 1:
+            if (intentName === "AnswerIntentA"){
+                houseTally[3].points += 5;
+            } else if (intentName === "AnswerIntentB"){
+                houseTally[0].points += 5;
+            } else  if (intentName === "AnswerIntentC"){
+                houseTally[1].points += 5;
+            }
+        break;
+
+        case 2:
+            if (intentName === "AnswerIntentA"){
+                houseTally[0].points += 5;
+            } else if (intentName === "AnswerIntentB"){
+                houseTally[3].points += 5;
+            } else  if (intentName === "AnswerIntentC"){
+                houseTally[2].points += 5;
+            }
+        break;
+
+        case 3:
+            if (intentName === "AnswerIntentA"){
+                houseTally[0].points += 5;
+            } else if (intentName === "AnswerIntentB"){
+                houseTally[2].points += 5
+            } else  if (intentName === "AnswerIntentC"){
+                houseTally[1].points += 5;
+            }
+        break;
+
+        case 4:
+            if (intentName === "AnswerIntentA"){
+                houseTally[2].points += 5;
+            } else if (intentName === "AnswerIntentB"){
+                houseTally[3].points += 5;
+            } else  if (intentName === "AnswerIntentC"){
+                houseTally[1].points += 5;
+            }
+        break;
+
+        case 5:
+            if (intentName === "AnswerIntentA"){
+                houseTally[3].points += 5;
+            } else if (intentName === "AnswerIntentB"){
+                houseTally[1].points += 5;
+            } else  if (intentName === "AnswerIntentC"){
+                houseTally[2].points += 5;
+            }
+        break;
+    }
+}
+
 
 
 function buildSpeechletResponse(say, shouldEndSession) {
